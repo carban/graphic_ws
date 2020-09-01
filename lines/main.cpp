59a8647c8270f22f11e7c11a9e310a26e8535a11
carban <murillo.carlos@correounivalle.edu.co>
2020-09-01 16:00:18 -0500

    method drawline

--- lines/main.cpp
+++ lines/main.cpp
@@ -5,42 +5,50 @@
 
 using namespace std;
 
-GLfloat x1=0.0,y_1=0.0,x2=0.8,y2=0.8;
+void drawLine(GLfloat x_1, GLfloat y_1, GLfloat x_2, GLfloat y_2, double length){
+
+    glVertex2f(x_1, y_1);
+    glVertex2f(x_2, y_2);
+
+    double m = (y_2 - y_1) / (x_2 - x_1) ;
+    double b = y_1 - (m * x_1); 
+
+    double increment = 1.0 / length;
+
+    GLfloat y = y_1;
+
+	
+
+    for (GLfloat i = x_1; i < x_2; i += increment){
+       cout << i << " " << y / length << endl;
+       glVertex2f(i, y / 10) ;
+       y = y + m;
+       //y = (m * i) + b;
+    }
+
+}
 
 void display(void) {
-     glClear(GL_COLOR_BUFFER_BIT);
-
-     glBegin(GL_POINTS);
-
-     glVertex2f(x1, y_1);
-     glVertex2f(x2, y2);
-
-     double m = (y2 - y_1) / (x2 - x1) ;
-
-     GLfloat y = y_1;
-
-     for (GLfloat i = x1; i < x2; i += 0.001){
-        cout << i << " " << y << endl;
-        glVertex2f(i, y / 1000) ;
-        y = y + m;
-     }
-
-     glEnd();
-     glFlush();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POINTS);
+    drawLine(0.0, 0.0, 0.8, 0.8, 10);
+    double l = 10;
+    drawLine(0.0, 0.0, 0.8, 0.0, l);
+    drawLine(0.0, 0.0, 0.8, 0.8, l);
+    drawLine(0.0, 0.0, 0.8, -0.8, l);
+    drawLine(-0.8, 0.8, 0.8, 0.8, l);
+    //drawLine(-0.8, 0.8, 0.0, 0.0, l);
+    //drawLine(-0.8, -0.8, 0.0, 0.0, l);
+    
+    glEnd();
+    glFlush();
 }
 
 int main(int argc, char* argv[]) {
-    //string titulo = "("+string(argv[1]) +","+ string(argv[2]) +") - ("+ string(argv[3]) +","+ string(argv[4]) + ")";
-    /*
-    x1 =atof(argv[1]);
-    y_1 = atof(argv[2]);
-    x2 = atof(argv[3]);
-    y2 = atof(argv[4]);
-    */
     glutInit(&argc, argv);
     glutInitDisplayMode( GLUT_RGB | GLUT_SINGLE);
     glutInitWindowSize(500, 500);
-    glutCreateWindow("asasa");
+    glutCreateWindow("Lines");
     glutDisplayFunc(display);
     glutMainLoop();
 

160894d76b33dec80e2e83a9c2ef2958caba81ca
carban <murillo.carlos@correounivalle.edu.co>
2020-09-07 22:58:15 -0500

    coding

--- lines/main.cpp
+++ lines/main.cpp
@@ -10,44 +10,59 @@ void drawLine(GLfloat x_1, GLfloat y_1, GLfloat x_2, GLfloat y_2, double length)
     glVertex2f(x_1, y_1);
     glVertex2f(x_2, y_2);
 
+    if (x_2 < x_1 ){
+        GLfloat auxx = x_1;
+        GLfloat auxy = y_1;
+        x_1 = x_2;
+        y_1 = y_2;
+        x_2 = auxx;
+        y_2 = auxy;
+    }
+
     double m = (y_2 - y_1) / (x_2 - x_1) ;
-    double b = y_1 - (m * x_1); 
+    //double b = y_1 - (m * x_1);
 
     double increment = 1.0 / length;
 
     GLfloat y = y_1;
 
-	
-
     for (GLfloat i = x_1; i < x_2; i += increment){
        cout << i << " " << y / length << endl;
-       glVertex2f(i, y / 10) ;
+       glVertex2f(i, y / length) ;
        y = y + m;
        //y = (m * i) + b;
     }
+    cout << "===========================" << endl;
 
 }
 
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POINTS);
-    drawLine(0.0, 0.0, 0.8, 0.8, 10);
-    double l = 10;
-    drawLine(0.0, 0.0, 0.8, 0.0, l);
-    drawLine(0.0, 0.0, 0.8, 0.8, l);
+
+    double l = 150;
+
+    drawLine(0.0, 0.0, 1.0, 1.0, l);
     drawLine(0.0, 0.0, 0.8, -0.8, l);
-    drawLine(-0.8, 0.8, 0.8, 0.8, l);
+
+    drawLine(0.0, 0.0, 0.6, 0.8, l);
+    drawLine(0.0, 0.0, 0.6, -0.8, l);
+
+    drawLine(0.4, 0.4, 0.8, 0.8, l);
+
+    //drawLine(-0.8, 0.8, 0.8, 0.8, l);
+    //drawLine(0.8, 0.8, 0.0, 0.0, l);
     //drawLine(-0.8, 0.8, 0.0, 0.0, l);
     //drawLine(-0.8, -0.8, 0.0, 0.0, l);
-    
+
     glEnd();
     glFlush();
 }
-
+/*
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
     glutInitDisplayMode( GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowSize(500, 500);
+    glutInitWindowSize(700, 700);
     glutCreateWindow("Lines");
     glutDisplayFunc(display);
     glutMainLoop();
@@ -55,3 +70,4 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+*/
---
+++ lines/second.cpp
@@ -0,0 +1,68 @@
+#include <GL/glut.h>
+#include<stdlib.h>
+#include<math.h>
+
+void myinit(void)
+{
+ glClearColor(1.0,1.0,1.0,0.0);
+ glMatrixMode(GL_PROJECTION);
+ gluOrtho2D (0.0,640.0,0.0,480.0);
+ }
+
+float xstart=-100,ystart=-100,xend=200,yend=200,step,xinc,yinc,x,y;
+
+int Round(float a)			//any x i.e 1>=x>=0.5 is rounded to 1
+{
+  if(a-int(a)>=0.5)
+  return int(a)+1;
+  else
+  return int(a);
+     }
+
+void drawline()
+     {
+
+       float ydiff = yend-ystart;
+       float xdiff = xend-xstart;
+       if (abs(xdiff) > abs(ydiff))
+        step = abs(xdiff);                //assign abs(xdiff) to step if xdiff>ydiff
+        else
+        step = abs(ydiff);              //assign abs(ydiff) to step if xdiff<ydiff
+
+        xinc = xdiff/step;            //assign xdiff/step to xinc
+        yinc = ydiff/step;           //assign ydiff/step to yinc
+
+        x = xstart;               //assign xstart to x
+        y = ystart;              //assign ystart to y
+
+for(int k=0;k<step;k++)
+      {
+       x = x+xinc;       // update x by xinc
+       y = y+yinc;
+	   glColor3f(0,0,1); // sets the current drawing (foreground) color to blue
+		 glPointSize(10); // sets the size of points to be drawn (in pixels)
+	   glBegin(GL_POINTS); // writes pixels on the frame buffer with the current drawing color
+       glVertex2i(Round(x),Round(y));//sets vertex
+          glEnd();
+	 }
+//glutPostRedisplay();
+
+
+  }
+
+void display2(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT); // clears the frame buffer and set values defined in glClearColor() function call
+    drawline();
+    glFlush(); // flushes the frame buffer to the screen
+}
+
+int main(int argc,char** argv)
+{
+	glutInit(&argc,argv);
+    glutInitWindowSize(700,700);//sets the width and height of the window in pixels
+    glutCreateWindow("DDA Line Drawing");//creates the window as specified by the user as above.
+    glutDisplayFunc(display2);//links the display event with the display event handler(display)
+	//myinit();
+    glutMainLoop();//loops the current event
+}
